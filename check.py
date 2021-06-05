3dac1fa75b1ac6515e6bef94a07f4791acc74de5
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-06-05 16:47:37 +0200

    Added more check functions to separate instructions

--- check.py
+++ check.py
@@ -7,26 +7,25 @@ Defines functions that checks if one of the player won.
 from constants import *
 
 
-def check_victory(board, player):
-    # Checking rows
+def check_rows(board, player):
     for x in range(3):
         for y in range(3):
             if board[x][y] != STATES[player]:
                 break
         else:
-            print()
             return True
 
-    # Checking columns
+
+def check_columns(board, player):
     for x in range(3):
         for y in range(3):
             if board[y][x] != STATES[player]:
                 break
         else:
-            print()
             return True
 
-    # Checking diagonals
+
+def check_diagonals(board, player):
     for i in range(3):
         if board[i][i] != STATES[player]:
             break
@@ -34,9 +33,15 @@ def check_victory(board, player):
         return True
 
     for i in range(3):
-        if board[3-i-1][i] != STATES[player]:
+        if board[3 - i - 1][i] != STATES[player]:
             break
     else:
         return True
 
+    return False
+
+
+def check_victory(board, player):
+    if check_rows(board, player) or check_columns(board, player) or check_diagonals(board, player):
+        return True
     return False
