9318ac5d65c3740decdd940be7fba708b3389701
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-06-05 15:21:03 +0200

    Created first step of game loop

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 __author__ = 'Eric-Nicolas'
 
-
 STATES = " XO"
 ROWS = COLUMNS = 3
 
@@ -18,13 +17,30 @@ def check_victory(board):
     return False
 
 
-def game_loop():
-    pass
+def game_loop(board):
+    player = 1
+    column = row = -1
+
+    print("Le joueur 1 possède les X. Le joueur 2 possède les O.")
+
+    while not check_victory(board):
+        print_board(board)
+        print("C'est le tour de joueur", player)
+        column = int(input("Entrez le numéro de la colonne : "))
+        row = int(input("Entrez le numéro de la ligne : "))
+        print("Vous avez joué la case :", (column, row))
+
+        board[column][row] = STATES[player]
+
+        if player == 1:
+            player = 2
+        else:
+            player = 1
 
 
 def main():
     board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-    print_board(board)
+    game_loop(board)
 
 
 if __name__ == '__main__':
