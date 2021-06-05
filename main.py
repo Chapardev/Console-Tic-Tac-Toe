bc15b2e59dbb77dc7b479cc3222308c0d64ad72f
Eric-Nicolas <randomimus.beakuzz@gmail.com>
2021-06-05 14:55:01 +0200

    Show the table

--- main.py
+++ main.py
@@ -1,8 +1,39 @@
 """Console Tic Tac Toe"""
 
+__author__ = 'Eric-Nicolas'
+
+
+STATES = " XO"
+DASHES = 13
+
+
+def print_line():
+    for _ in range(DASHES):
+        print('-', end='')
+
+
+def show_table(x, y):
+    print('\t', end='')
+    for i in range(x):
+        print(' ' + str(i) + ') ', end=' ')
+    print()
+    for i in range(y):
+        print('\t', end='')
+        print_line()
+        print('\n' + str(i) + ')  ', end='')
+        for j in range(DASHES):
+            if j % 4 == 0:
+                print('|', end='')
+            else:
+                print(' ', end='')
+        print()
+    print('\t', end='')
+    print_line()
+
 
 def main():
     print("Hello World!")
+    show_table(3, 3)
 
 
 if __name__ == '__main__':
